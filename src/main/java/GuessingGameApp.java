f92f8d5328c7ae7d25ed4f09f5e880cd108f0dbf
Branden Webb <brandenswebb@hotmail.com>
2021-01-21 16:43:58 -0500

    iteration 1

--- src/main/java/GuessingGameApp.java
+++ src/main/java/GuessingGameApp.java
@@ -1,6 +1,20 @@
+import java.util.*;
+
 public class GuessingGameApp {
 
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int randomNumber = 7;
+        int guessedNumber = 0;
+
+        System.out.print("Welcome to the Guessing Game! Please Guess a number between 1 and 10: ");
+        guessedNumber = in.nextInt();
+        if(guessedNumber == randomNumber) {
+            System.out.println("You win! " + randomNumber + " was the number!");
+        }
+        else {
+            System.out.println("You lose! " + randomNumber + " was the number !");
+        }
 
     }
 
