0ce18c0e5af7195ea9c7181a4ffc4dfe8c14e10c
Branden Webb <brandenswebb@hotmail.com>
2021-01-21 16:50:28 -0500

    iteration 2

--- src/main/java/GuessingGameApp.java
+++ src/main/java/GuessingGameApp.java
@@ -1,3 +1,4 @@
+import java.sql.SQLOutput;
 import java.util.*;
 
 public class GuessingGameApp {
@@ -7,13 +8,19 @@ public class GuessingGameApp {
         int randomNumber = 7;
         int guessedNumber = 0;
 
-        System.out.print("Welcome to the Guessing Game! Please Guess a number between 1 and 10: ");
-        guessedNumber = in.nextInt();
+        System.out.print("Welcome to the Guessing Game! Please Guess a number between 1 and 10, or type 0 for instructions: ");
+        do {
+            guessedNumber = in.nextInt();
+
+            if(guessedNumber == 0) {
+                System.out.println("The goal of the game is to guess the random number between 1 and 10. Good Luck!");
+            }
+        } while (guessedNumber == 0);
         if(guessedNumber == randomNumber) {
             System.out.println("You win! " + randomNumber + " was the number!");
         }
         else {
-            System.out.println("You lose! " + randomNumber + " was the number !");
+            System.out.println("You lose! " + randomNumber + " was the number!");
         }
 
     }
